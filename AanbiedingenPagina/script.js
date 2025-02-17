d5865f9a37171b08650efa17bc18b6d13f593ea7
AshDashTooFast3 <338127@student.mboutrecht.nl>
2025-02-17 20:04:53 +0100

    Delete AanbiedingenPagina directory

--- AanbiedingenPagina/script.js
+++
@@ -1,8 +0,0 @@
-// de foutmelding:
-window.addEventListener('error', function() {
-    alert('Er is een fout opgetreden bij het laden van de pagina. Probeer het later opnieuw.');
-    window.location.href = 'Homepagina/index.html';
-});
-
-//een fout veroorzaken
-//nonExistentFunction();
