ebf28c1a3e990daf1a8269215721834718ff6ea3
Achraf El Arrasi <338127@student.mboutrecht.nl>
2025-02-17 17:24:56 +0100

    Foutmelding aangemaakt

---
+++ AanbiedingenPagina/script.js
@@ -0,0 +1,8 @@
+// de foutmelding:
+window.addEventListener('error', function() {
+    alert('Er is een fout opgetreden bij het laden van de pagina. Probeer het later opnieuw.');
+    window.location.href = 'Homepagina/index.html';
+});
+
+// Opzettelijk een fout veroorzaken
+nonExistentFunction();
