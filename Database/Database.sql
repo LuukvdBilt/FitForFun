5d41d1fde0f8a6ec3822ceeb493617f56cebed63
Wesley <340308@student.mboutrecht.nl>
2025-02-20 21:23:46 +0100

    SQL SCRIPT

---
+++ Database/Database.sql
@@ -0,0 +1,43 @@
+<?php
+-- Hier onder staat de SQL code die de database aanmaakt en de tabellen met de gegevens
+-- Als je iets verandert aan de database, zet hier dan het laatste script in!
+-- Als je meer naar beneden scrolt gaan we de verbinding maken met de database in Phpmyadmin
+
+--SQL:
+
+DROP DATABASE IF EXISTS FitForFun;
+
+CREATE DATABASE FitForFun;
+
+use Fitforfun; 
+
+CREATE TABLE Aanbiedingen (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    beschrijving TEXT NOT NULL,
+    prijs DECIMAL(10, 2) NOT NULL,
+    kortingsprijs DECIMAL(10, 2) NOT NULL
+);
+
+INSERT INTO Aanbiedingen (beschrijving, prijs, kortingsprijs) VALUES
+('🏋️‍♂️ FitForFun Deal: 3 Maanden Sportschoolabonnement 📌 Krijg 3 maanden onbeperkt toegang tot de sportschool 💰 Nu voor slechts', 80.00, 80.00 * 0.70),
+('🏃‍♀️ FitForFun Deal: 2 Hardloopschoenen Voor de Prijs van 1! 📌 Koop één paar hardloopschoenen en krijg de tweede gratis!', 80.00, 40.00),
+('🚴 FitForFun Deal: 30% Korting op Sportkleding en Accessoires! 📌 Van sportleggings tot fietshandschoenen-shop nu met korting!', 50.00, 50.00 * 0.70);
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('Administrator','Medewerker','Lid', 'GastGebruiker') NOT NULL
+);
+
+
+INSERT INTO users (username, password, role) VALUES
+('admin', 'Achraf1532!?', 'Administrator'),
+('Wesley', 'BorgMan5289!', 'MedeWerker'),
+('Luuk', 'vdBilt0184?', 'MedeWerker'),
+('Yassine', 'ElBardai6732*', 'MedeWerker'),
+('Jordy', 'JordyPass123!', 'GastGebruiker'),
+('Thijs', 'ThijsPass456!', 'GastGebruiker'),
+('Jeroen', 'JeroenPass789!', 'Lid'),
+('Jesse', 'JessePass012!', 'Lid');
+?>
