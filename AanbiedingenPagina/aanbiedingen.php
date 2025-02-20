2ac18dcdbd74d77e95f44fa6a17a93f2cffae96e
Achraf El Arrasi <338127@student.mboutrecht.nl>
2025-02-20 14:19:29 +0100

    code opschonen +horziontale scrollbar verborgen

--- AanbiedingenPagina/aanbiedingen.php
+++ AanbiedingenPagina/aanbiedingen.php
@@ -6,9 +6,8 @@ $dbname = "Aanbiedingen";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 ?>
 
@@ -22,7 +21,8 @@ if ($conn->connect_error) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css"
+    rel="stylesheet">
 </head>
 
 <body>
@@ -30,42 +30,41 @@ if ($conn->connect_error) {
     <div class="row">
       <div class="col-12">
         <nav class="navbar navbar-expand-lg">
-          <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <img class="logo"
-                src="https://www.burda-forward.de/files/images/03_Media/Brands/FitForFun/BF_Media_Brands_FitForFun_logo.png"
-                alt="">
-              <ul class="navbar-nav me-auto">
-                <li class="nav-item"></li>
-                <a class="nav-link" href="index.html">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="aanbiedingen.html">Aanbiedingen</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="lessen.html">Geplande lessen</a>
-                </li>
-                <li>
-                  <a class="nav-link" href="overzicht.html">Overzicht</a>
-                </li>
-              </ul>
-              <ul class="navbar-nav">
-                <a class="nav-link" href="#"><i class="bi bi-cart"></i> Winkelwagen</a>
-                <a class="nav-link" href="#">Login</a>
-                </li>
-                <li>
-                  <a class="nav-link" href="#">Register</a>
-                </li>
-              </ul>
-            </div>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <img class="logo"
+              src="https://www.burda-forward.de/files/images/03_Media/Brands/FitForFun/BF_Media_Brands_FitForFun_logo.png"
+              alt="">
+            <ul class="navbar-nav me-auto">
+              <li class="nav-item"></li>
+              <a class="nav-link" href="index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="aanbiedingen.html">Aanbiedingen</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="lessen.html">Geplande lessen</a>
+              </li>
+              <li>
+                <a class="nav-link" href="overzicht.html">Overzicht</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav">
+              <a class="nav-link" href="#"><i class="bi bi-cart"></i> Winkelwagen</a>
+              <a class="nav-link" href="#">Login</a>
+              </li>
+              <li>
+                <a class="nav-link" href="#">Register</a>
+              </li>
+            </ul>
           </div>
-        </nav>
       </div>
+      </nav>
     </div>
+  </div>
   </div>
   <h1 class="h1">Aanbiedingen</h1>
   <div class="row">
@@ -74,7 +73,6 @@ if ($conn->connect_error) {
     $sql = "SELECT beschrijving, prijs, kortingsprijs FROM Aanbiedingen";
     $result = $conn->query($sql);
 
-    // Definieer de emoji's waarvoor je een <br> wilt invoegen
     $emojiArray = [
       "🏋️‍♂️",
       "📌",
@@ -86,7 +84,6 @@ if ($conn->connect_error) {
 
     if ($result->num_rows > 0) {
       while ($row = $result->fetch_assoc()) {
-        // Pas de emoji vervanging toe op de beschrijving
         $beschrijving = $row["beschrijving"];
         foreach ($emojiArray as $emoji) {
           $beschrijving = str_replace($emoji, $emoji . "<br>", $beschrijving);
@@ -99,23 +96,21 @@ if ($conn->connect_error) {
         echo "    <button class='btn'>Voeg aanbieding toe</button>";
         echo "  </div>";
         echo "</div>";
-            }
-          } else {
-      echo "0 results";
+      }
+    } else {
+      echo "geen aanbiedingen gevonden";
     }
     $conn->close();
 
     ?>
   </div>
-
   <strong>
     <h2 class="WARNING">LET OP: je kunt maar 1 aanbieding gebruiken!</h2>
   </strong>
-
   <script src="script.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-  </script>
+    </script>
 </body>
 
 </html>
