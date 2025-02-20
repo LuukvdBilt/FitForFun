e1d871b74df3985dae29bf7e676cd4d00b27d49e
Achraf El Arrasi <338127@student.mboutrecht.nl>
2025-02-20 15:08:01 +0100

    onnodige tekst verwijderd

--- AanbiedingenPagina/aanbiedingen.sql
+++ AanbiedingenPagina/aanbiedingen.sql
@@ -13,5 +13,5 @@ CREATE TABLE Aanbiedingen (
 
 INSERT INTO Aanbiedingen (beschrijving, prijs, kortingsprijs) VALUES
 ('🏋️‍♂️ FitForFun Deal: 3 Maanden Sportschoolabonnement 📌 Krijg 3 maanden onbeperkt toegang tot de sportschool 💰 Nu voor slechts', 80.00, 80.00 * 0.70),
-('🏃‍♀️ FitForFun Deal: 2 Hardloopschoenen Voor de Prijs van 1! 📌 Koop één paar hardloopschoenen en krijg de tweede gratis. 🎁 Perfect voor jou en je sportmaatje!', 80.00, 40.00),
-('🚴 FitForFun Deal: 30% Korting op Sportkleding en Accessoires! 📌 Van sportleggings tot fietshandschoenen—shop nu met korting! 🛒 Gebruik code: FIT30 bij het afrekenen!', 50.00, 50.00 * 0.70);
+('🏃‍♀️ FitForFun Deal: 2 Hardloopschoenen Voor de Prijs van 1! 📌 Koop één paar hardloopschoenen en krijg de tweede gratis!', 80.00, 40.00),
+('🚴 FitForFun Deal: 30% Korting op Sportkleding en Accessoires! 📌 Van sportleggings tot fietshandschoenen-shop nu met korting!', 50.00, 50.00 * 0.70);
