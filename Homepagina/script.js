66719eebdf66c935e111dd60ca3a0847b2d3be91
Wesley <340308@student.mboutrecht.nl>
2025-02-21 09:53:45 +0100

    links

--- Homepagina/script.js
+++ Homepagina/script.js
@@ -5,3 +5,16 @@ navLink.addEventListener('click', function(){
     window.location.href = 'index.php';
     alert('U bevindt zich al plaats op de Homepagina');
 });
+
+
+const btnCards = document.querySelectorAll('.btncard');
+
+btnCards.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        if (index === 0) {
+            window.location.href = 'aanbiedingen.php';
+        } else if (index === 1) {
+            window.location.href = 'lessen.php';
+        }
+    });
+});
