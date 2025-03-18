a3d5a46bd170ae00813367d451c317f3633ac9cc
Achraf El Arrasi <338127@student.mboutrecht.nl>
2025-03-18 10:55:33 +0100

    Tabbelen van feature branch toegvoegd aan de goede database

--- Database/Database.sql
+++ Database/Database.sql
@@ -147,3 +147,119 @@ VALUES
 ,('Lars', NULL, 'Moore', '2025-02-05')
 ,('Sofia', NULL, 'Jackson', '2025-02-10')
 ,('Ming', NULL, 'Martin', '2025-02-15');
+
+CREATE TABLE LedenOverzicht
+(
+     Id              TINYINT             UNSIGNED           NOT NULL   AUTO_INCREMENT
+    ,Voornaam        VARCHAR(150)                           NOT NULL
+    ,Tussenvoegsel   VARCHAR(50)                            NULL
+    ,Achternaam      VARCHAR(150)                           NOT NULL
+    ,Leeftijd        TINYINT UNSIGNED                       NOT NULL
+    ,Geslacht        VARCHAR(180)                           NOT NULL   
+    ,IsActief        BIT                                    NOT NULL    DEFAULT 1
+    ,Opmerking       VARCHAR(255)                           NULL        DEFAULT NULL
+    ,DatumAangemaakt DATETIME(6)                            NOT NULL    
+    ,DatumGewijzigd  DATETIME(6)                            NOT NULL
+    ,CONSTRAINT      PK_LedenOverzicht_Id                   PRIMARY KEY CLUSTERED(Id)
+) ENGINE=InnoDB;
+
+
+INSERT INTO LedenOverzicht
+(
+         Voornaam
+        ,Tussenvoegsel
+        ,Achternaam
+        ,Leeftijd
+        ,Geslacht
+        ,IsActief
+        ,Opmerking
+        ,DatumAangemaakt
+        ,DatumGewijzigd
+)
+VALUES
+('Jan', NULL, 'Jansen', 25, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Sanne', 'De', 'Vries', 30, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Pieter', 'van', 'Dijk', 22, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Emma', NULL, 'Bakker', 28, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Lucas', NULL, 'Visser', 35, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Sophie', 'Van', 'Smit', 27, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Daan', 'de', 'Boer', 32, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Lotte', NULL, 'Mulder', 24, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Thomas', NULL, 'Dekker', 29, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Eva', NULL, 'Kok', 26, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Ruben', NULL, 'Meijer', 31, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Anna', 'van', 'Groot', 23, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Jesse', NULL, 'Bos', 34, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Julia', NULL, 'Vos', 25, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Lars', NULL, 'Peters', 28, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Mila', NULL, 'Hendriks', 27, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Finn', NULL, 'Bruin', 33, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Noa', NULL, 'Leeuw', 22, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Sem', 'De', 'Koning', 30, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Tess', NULL, 'Schouten', 24, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Milan', NULL, 'Graaf', 29, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Fleur', NULL, 'Lange', 26, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Niels', NULL, 'Ruiter', 28, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Roos', 'De', 'Wit', 27, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Sven', NULL, 'Haan', 31, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Lieke', NULL, 'Jong', 25, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Timo', NULL, 'Boer', 32, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Sara', NULL, 'Vos', 24, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Joep', NULL, 'Vries', 29, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Lina', 'Van der', 'Groot', 26, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Teun', NULL, 'Bruin', 33, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Maud', 'De', 'Leeuw', 22, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Rik', NULL, 'Koning', 30, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Lara', NULL, 'Schouten', 24, 'F', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Joris', NULL, 'Graaf', 28, 'M', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Nina', NULL, 'Lange', 27, 'F', 1, NULL, SYSDATE(6), SYSDATE(6));
+
+CREATE TABLE MedewerkerOverzicht
+(
+    Id               TINYINT(3)          UNSIGNED           NOT NULL   AUTO_INCREMENT
+    ,Voornaam        VARCHAR(150)                           NOT NULL
+    ,Tussenvoegsel   VARCHAR(50)                            NULL
+    ,Achternaam      VARCHAR(150)                           NOT NULL
+    ,Telefoonnummer  VARCHAR(180)                           NOT NULL
+    ,Werknemerrank   VARCHAR(180)                           NOT NULL
+    ,IsActief        BIT                                    NOT NULL    DEFAULT 1
+    ,Opmerking       VARCHAR(255)                           NULL        DEFAULT NULL
+    ,DatumAangemaakt DATETIME(6)                            NOT NULL    
+    ,DatumGewijzigd  DATETIME(6)                            NOT NULL
+    ,CONSTRAINT      PK_MedewerkerOverzicht_Id              PRIMARY KEY CLUSTERED(Id)
+) ENGINE=InnoDB;
+
+INSERT INTO MedewerkerOverzicht
+(
+         Voornaam
+        ,Tussenvoegsel
+        ,Achternaam
+        ,Telefoonnummer
+        ,Werknemerrank
+        ,IsActief
+        ,Opmerking
+        ,DatumAangemaakt
+        ,DatumGewijzigd
+)
+VALUES
+('Ahmed', NULL, 'Khasmiri', '0634127345', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Ismael', 'El', 'Karamari', '0670236044', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Wessel', 'Van der', 'Meer', '0699102045', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Mark', NULL, 'Zuckerberg', '0683213481', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Daniel', NULL, 'Zegveld', '0636936578', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Sophie', NULL, 'Jansen', '0612345678', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Lucas', 'De', 'Vries', '0623456789', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Emma', NULL, 'Bakker', '0634567890', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Mila', 'Van', 'Dijk', '0645678901', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Noah', NULL, 'Visser', '0656789012', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Eva', NULL, 'Smit', '0667890123', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Liam', 'Van', 'Leeuwen', '0678901234', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Julia', NULL, 'Dekker', '0689012345', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Finn', 'De', 'Jong', '0690123456', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Tess', NULL, 'Bos', '0601234567', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Carlos', NULL, 'Garcia', '0612345678', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Yuki', NULL, 'Tanaka', '0623456789', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Aisha', NULL, 'Khan', '0634567890', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Lars', NULL, 'Hansen', '0645678901', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Sofia', NULL, 'Rossi', '0656789012', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Ming', NULL, 'Li', '0667890123', '2', 1, NULL, SYSDATE(6), SYSDATE(6));
