24e7b4169370d5798ef4b6df7cb75a2b89d3cbe4
Wesley <340308@student.mboutrecht.nl>
2025-03-21 09:52:11 +0100

    wesley is de goat

--- Dashboard/Dashboard.php
+++ Dashboard/Dashboard.php
@@ -12,47 +12,47 @@
 
 <body>
   <h1>Management Dashboard</h1>
-<?php
- echo str_repeat("<br>", 2);
-?>
+  <?php
+  echo str_repeat("<br>", 2);
+  ?>
   <!-- onze navbar !-->
   <nav class="navbar navbar-expand-lg fixed-top">
-        <div class="container-fluid">
-            <img class="logo"
-              src="https://www.burda-forward.de/files/images/03_Media/Brands/FitForFun/BF_Media_Brands_FitForFun_logo.png"
-              alt="FitForFun Logo">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-            <a class="nav-link nav-link-Home" href="../Homepagina/index.php">Home</a>
+    <div class="container-fluid">
+      <img class="logo"
+        src="https://www.burda-forward.de/files/images/03_Media/Brands/FitForFun/BF_Media_Brands_FitForFun_logo.png"
+        alt="FitForFun Logo">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="../Homepagina/index.php">Home</a>
           </li>
           <li class="nav-item">
-          <a class="nav-link" href="../AanbiedingenPagina/aanbiedingen.php">Aanbiedingen</a>
+            <a class="nav-link" href="../AanbiedingenPagina/aanbiedingen.php">Aanbiedingen</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="../lessonkalender/lessen.php">Geplande lessen</a>
+            <a class="nav-link" href="../LesOverzicht/public/lessen.php">Geplande lessen</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="../Dashboard/Dashboard.php">Management Dashboard</a>
+            <a class="nav-link" href="../AccountsOverzicht/login.php">Management Dashboard</a>
           </li>
-            </ul>
-    
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="#">Login</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="PerPeriode/AccountPerPeriode.php">Register</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+        </ul>
+
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="PerPeriode/AccountPerPeriode.php">Register</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
   <div class="container-fluid">
     <div class="row">
@@ -66,10 +66,10 @@
         </div>
       </div>
       <div class="col-4"></div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-      crossorigin="anonymous"></script>
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
--- Database/Database.sql
+++ Database/Database.sql
@@ -159,3 +159,55 @@ VALUES
 ('Sofia', 'de', 'Harris', 'REL022', '0623456789', 'sofia.harris@example.com', 1, NULL, SYSDATE(6), SYSDATE(6)),
 ('Ming', NULL, 'Martin', 'REL023', '0634567890', 'ming.martin@example.com', 1, NULL, SYSDATE(6), SYSDATE(6)),
 ('Mingo', 'van der', 'Lee', 'REL024', '0645678901', 'mingo.lee@example.com', 1, NULL, SYSDATE(6), SYSDATE(6));
+
+CREATE TABLE Les (
+    Id INT NOT NULL AUTO_INCREMENT,
+    Naam VARCHAR(50) NOT NULL,
+    Datum DATE NOT NULL,
+    Tijd TIME NOT NULL,
+    MinAantalPersonen TINYINT NOT NULL,
+    MaxAantalPersonen TINYINT NOT NULL,
+    Beschikbaarheid VARCHAR(50) NOT NULL,
+    Prijs DECIMAL(10, 2) NOT NULL, -- Toegevoegd Prijs kolom
+    Isactief BIT NOT NULL,
+    Opmerking VARCHAR(250),
+    Datumaangemaakt DATETIME(6) NOT NULL,
+    Datumgewijzigd DATETIME(6) NOT NULL,
+    PRIMARY KEY (Id)
+);
+
+-- Create the Reservering table
+CREATE TABLE Reservering (
+    Id INT NOT NULL AUTO_INCREMENT,
+    Voornaam VARCHAR(50) NOT NULL,
+    Tussenvoegsel VARCHAR(10),
+    Achternaam VARCHAR(50) NOT NULL,
+    Nummer MEDIUMINT NOT NULL,
+    LesId INT NOT NULL, -- Toegevoegd LesId kolom
+    Datum DATE NOT NULL,
+    Tijd TIME NOT NULL,
+    Reserveringstatus VARCHAR(20) NOT NULL,
+    Isactief BIT NOT NULL,
+    Opmerking VARCHAR(250),
+    Datumaangemaakt DATETIME(6) NOT NULL,
+    Datumgewijzigd DATETIME(6) NOT NULL,
+    PRIMARY KEY (Id),
+    FOREIGN KEY (LesId) REFERENCES Les(Id) -- Toegevoegd foreign key constraint
+);
+
+-- Voorbeeldgegevens invoegen
+INSERT INTO Les (Id, Naam, Datum, Tijd, Beschikbaarheid, MinAantalPersonen, MaxAantalPersonen, Prijs, Isactief, Opmerking, Datumaangemaakt, Datumgewijzigd)
+VALUES
+(1, 'Yoga', '2023-10-05', '09:00:00', 'Ingepland', 3, 9, 15.00, 1, 'Breng een matje mee.', '2023-09-01 10:00:00', '2023-09-01 10:00:00'),
+(2, 'Spinning', '2023-10-06', '10:30:00', 'Ingepland', 2, 8, 20.00, 1, 'Intensieve sessie.', '2023-09-02 11:00:00', '2023-09-02 11:00:00'),
+(3, 'Pilates', '2023-10-07', '14:00:00', 'Ingepland', 4, 10, 18.00, 1, 'Geschikt voor alle niveaus.', '2023-09-03 12:00:00', '2023-09-03 12:00:00'),
+(4, 'Zumba', '2023-10-08', '18:00:00', 'Ingepland', 5, 12, 22.00, 1, 'Dansen en fit blijven!', '2023-09-04 13:00:00', '2023-09-04 13:00:00'),
+(5, 'Kickboksen', '2023-10-09', '19:30:00', 'Ingepland', 3, 10, 25.00, 1, 'Voor zowel beginners als gevorderden.', '2023-09-05 14:00:00', '2023-09-05 14:00:00');
+
+INSERT INTO Reservering (Id, Voornaam, Tussenvoegsel, Achternaam, Nummer, LesId, Datum, Tijd, Reserveringstatus, Isactief, Opmerking, Datumaangemaakt, Datumgewijzigd)
+VALUES
+(1, 'Jan', 'van', 'Janssen', 123456789, 1, '2023-10-05', '09:00:00', 'Gereserveerd', 1, 'Eerste keer Yoga.', '2023-09-06 08:00:00', '2023-09-06 08:00:00'),
+(2, 'Piet', '', 'Hein', 987654321, 2, '2023-10-06', '10:30:00', 'Gereserveerd', 1, 'Regelmatige spinner.', '2023-09-07 09:00:00', '2023-09-07 09:00:00'),
+(3, 'Klaas', 'de', 'Boer', 456789123, 3, '2023-10-07', '14:00:00', 'Gereserveerd', 1, 'Pilates liefhebber.', '2023-09-08 10:00:00', '2023-09-08 10:00:00'),
+(4, 'Griet', 'van', 'Buren', 789123456, 4, '2023-10-08', '18:00:00', 'Gereserveerd', 1, 'Liefhebber van Zumba.', '2023-09-09 11:00:00', '2023-09-09 11:00:00'),
+(5, 'Anne', 'de', 'Jong', 321654987, 5, '2023-10-09', '19:30:00', 'Gereserveerd', 1, 'Kickboksen voor conditie.', '2023-09-10 12:00:00', '2023-09-10 12:00:00');
