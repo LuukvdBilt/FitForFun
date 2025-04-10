6fcc85ed5b7c11521f22b7a19982a086e043f14e
yassine-el-B <340524@student.mboutrecht.nl>
2025-04-10 16:10:30 +0200

    hier

--- LesOverzicht/public/lessen.php
+++ LesOverzicht/public/lessen.php
@@ -30,11 +30,51 @@ $resultReservering = $conn->query($sqlReservering);
     <title>Fitness Website</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
    
     
 </head>
 <body>
+<nav class="navbar navbar-expand-lg fixed-top">
+    <div class="container-fluid">
+      <img class="logo"
+        src="https://www.burda-forward.de/files/images/03_Media/Brands/FitForFun/BF_Media_Brands_FitForFun_logo.png"
+        alt="FitForFun Logo">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="../../Homepagina/index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../AanbiedingenPagina/aanbiedingen.php">Aanbiedingen</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../LesOverzicht/public/lessen.php">Geplande lessen</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../AccountsOverzicht/login.php">Management Dashboard</a>
+          </li>
+        </ul>
+
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="PerPeriode/AccountPerPeriode.php">Register</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container-fluid">
       <img class="logo"
@@ -208,6 +248,9 @@ $resultReservering = $conn->query($sqlReservering);
             return new bootstrap.Tooltip(tooltipTriggerEl)
         })
     </script>
+    <script id="lesData" type="application/json"><?= json_encode($resultLes->fetch_all(MYSQLI_ASSOC)) ?></script>
+<script id="reserveringData" type="application/json"><?= json_encode($resultReservering->fetch_all(MYSQLI_ASSOC)) ?></script>
+
 </body>
 </html>
 
