3693d22405b6b99d81295bbb6d87e11796c52a97
Wesley <340308@student.mboutrecht.nl>
2025-04-10 13:22:19 +0200

    ledenoverzicht

--- LedenOverzicht/LedenOverzicht.php
+++ LedenOverzicht/LedenOverzicht.php
@@ -33,7 +33,7 @@ $result = $statement->fetchAll(PDO::FETCH_OBJ);
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg fixed-top">
+  <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container-fluid">
       <img class="logo"
         src="https://www.burda-forward.de/files/images/03_Media/Brands/FitForFun/BF_Media_Brands_FitForFun_logo.png"
@@ -72,12 +72,12 @@ $result = $statement->fetchAll(PDO::FETCH_OBJ);
   </nav>
   
   <div class="background">
-  <button class="createbutton" onclick="window.location.href='create.php'">Nieuw lid toevoegen</button>
-  <button class="searchbutton" onclick="toggleSearch()">zoek op achternaam</button>
-  <input type="text" id="searchInput" placeholder="Enter Achternaam" class="search-input" onkeyup="searchTable()"/>
-  <div id="noResultsAlert" class="alert alert-danger" role="alert" style="display: none;">
-    <span>Geen overeenkomende achternaam gevonden.</span>
-  </div>
+    <button class="createbutton" onclick="window.location.href='create.php'">Nieuw lid toevoegen</button>
+    <button class="searchbutton" onclick="toggleSearch()">zoek op achternaam</button>
+    <input type="text" id="searchInput" placeholder="Enter Achternaam" class="search-input" onkeyup="searchTable()" />
+    <div id="noResultsAlert" class="alert alert-danger" role="alert" style="display: none;">
+      <span>Geen overeenkomende achternaam gevonden.</span>
+    </div>
 
     <h1>Leden Overzicht</h1>
     <table border="1">
@@ -89,6 +89,7 @@ $result = $statement->fetchAll(PDO::FETCH_OBJ);
         <th>Mobiel</th>
         <th>Email</th>
         <th>Edit</th>
+        <th>Delete</th>
       </tr>
       <?php foreach ($result as $row): ?>
         <tr>
@@ -101,6 +102,16 @@ $result = $statement->fetchAll(PDO::FETCH_OBJ);
           <td>
             <a href="edit.php?Relatienummer=<?php echo urlencode($row->Relatienummer); ?>" class="btn btn-primary btn-sm">Edit</a>
           </td>
+            <td>
+            <form action="delete.php" method="POST" onsubmit="return confirm('Weet je zeker dat je dit lid wilt verwijderen?');">
+              <input type="hidden" name="Relatienummer" value="<?php echo htmlspecialchars($row->Relatienummer); ?>">
+              <button type="submit" class="btn btn-danger btn-sm">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+              </svg>
+              </button>
+            </form>
+            </td>
         </tr>
       <?php endforeach; ?>
     </table>
